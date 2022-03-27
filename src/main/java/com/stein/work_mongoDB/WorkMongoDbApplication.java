fb750acd638becb2426c81b75ebff28e797ccc97
stein <igorxdev@gmail.com>
2022-03-27 11:08:18 -0300

    project created

---
+++ src/main/java/com/stein/work_mongoDB/WorkMongoDbApplication.java
@@ -0,0 +1,13 @@
+package com.stein.work_mongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stein/work_mongoDB/WorkMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stein.work_mongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
